c6c58a64b8a9c047f99137c33fc59da83bcb2f18
Massimo Galleri <massimo@galleri.it>
2019-02-04 13:29:15 +0100

    Update parallax-hero.js

--- parallax-hero.js
+++ parallax-hero.js
@@ -1,56 +1,60 @@
-// vars
+{
+    
+    // vars
 
-var _window = document.documentElement,
-    _body = document.querySelector('body'),
-    _hero = document.getElementsByClassName('hero'),
-    _image_bg = document.getElementsByClassName('image-bg'),
-    _video_bg = document.getElementsByClassName('video-bg'),
-    _hero_content = document.getElementsByClassName('hero-content');
+    const _window = document.documentElement;
+    const _body = document.querySelector('body');
+    const _hero = document.getElementsByClassName('hero');
+    const _image_bg = document.getElementsByClassName('image-bg');
+    const _video_bg = document.getElementsByClassName('video-bg');
+    const _hero_content = document.getElementsByClassName('hero-content');
 
 
-// core function
+    // core function
 
-function _parallax_hero() {
+    function _parallax_hero() {
 
-    var _current_top = _window.scrollTop;
+        let _current_top = _window.scrollTop;
 
-    // image-bg 
+        // image-bg 
 
-    if (_image_bg.length > 0) {
-        for (var i = 0; i < _image_bg.length; i++) {
-            // option translate
-            _image_bg[i].style.transform = 'translateY(' + _current_top * 0.5 + 'px)';
-            // option scale
-            _image_bg[i].style.transform = 'scale(' + (_current_top / 1000 + 1) + ')';
-            // option opacity
-            _image_bg[i].style.opacity = 1 - _current_top / 1000;
+        if (_image_bg.length > 0) {
+            for (var i = 0; i < _image_bg.length; i++) {
+                // option translate
+                _image_bg[i].style.transform = 'translateY(' + _current_top * 0.5 + 'px)';
+                // option scale
+                _image_bg[i].style.transform = 'scale(' + (_current_top / 1000 + 1) + ')';
+                // option opacity
+                _image_bg[i].style.opacity = 1 - _current_top / 1000;
+            }
         }
+
+        // video-bg 
+
+        if (_video_bg.length > 0) {
+            for (var i = 0; i < _video_bg.length; i++) {
+                // option translate
+                _video_bg[i].style.transform = 'translateY(' + _current_top * 0.5 + 'px)';
+                // option scale
+                _video_bg[i].style.transform = 'scale(' + (_current_top / 1000 + 1) + ')';
+                // option opacity
+                _video_bg[i].style.opacity = 1 - _current_top / 1000;
+            }
+        }
+
+        // hero-content
+
+        if (_hero_content.length > 0) {
+            for (var i = 0; i < _hero_content.length; i++) {
+                // option opacity
+                _hero_content[i].style.opacity = 1 - _current_top / 500;
+            }
+        }
+
     }
 
-    // video-bg 
+    _parallax_hero();
 
-    if (_video_bg.length > 0) {
-        for (var i = 0; i < _video_bg.length; i++) {
-            // option translate
-            _video_bg[i].style.transform = 'translateY(' + _current_top * 0.5 + 'px)';
-            // option scale
-            _video_bg[i].style.transform = 'scale(' + (_current_top / 1000 + 1) + ')';
-            // option opacity
-            _video_bg[i].style.opacity = 1 - _current_top / 1000;
-        }
-    }
-
-    // hero-content
-
-    if (_hero_content.length > 0) {
-        for (var i = 0; i < _hero_content.length; i++) {
-            // option opacity
-            _hero_content[i].style.opacity = 1 - _current_top / 500;
-        }
-    }
-
+    window.addEventListener('scroll', _parallax_hero);
+    
 }
-
-_parallax_hero();
-
-window.addEventListener('scroll', _parallax_hero);
